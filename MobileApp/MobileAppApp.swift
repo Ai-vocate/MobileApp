632e5aeb029e076c2cb5badf94c7f7a2afa2fcdb
Amokhalad <mjayalju12234@gmail.com>
2023-06-02 23:15:54 -0700

    Initial Commit

---
+++ MobileApp/MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppApp.swift
+//  MobileApp
+//
+//  Created by Mokhalad on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
