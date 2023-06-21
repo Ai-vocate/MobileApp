b7c1b96779b1983a616c3580212a3164dd6eebe2
“Sean <sean.she@berkeley.edu>
2023-06-21 08:43:06 -0700

    tab bar + other views

---
+++ MobileApp/Views/Components/TabBar.swift
@@ -0,0 +1,49 @@
+//
+//  TabBar.swift
+//  MobileApp
+//
+//  Created by Sean She on 6/21/23.
+//
+
+// Resources
+// https://www.youtube.com/watch?v=vzQDKYIKEb8&ab_channel=Indently
+// https://www.youtube.com/watch?v=Yg3cmpKNieU&ab_channel=ASwiftlyTiltingPlanet
+// https://www.youtube.com/watch?v=TgvYFfCjDMo&t=238s&ab_channel=Indently
+
+// TODO: Connect TabBar to whatever is the landing view after sign in
+
+import SwiftUI
+
+let tabBarItems = ["house", "questionmark", "bubble.left.and.bubble.right", "clock.arrow.circlepath"]
+
+
+struct TabBar: View {
+    
+    @Binding var selectedTab: String
+    
+    var body: some View {
+        HStack {
+            ForEach(tabBarItems, id: \.self) { tab in
+                Spacer()
+                Image(systemName: tab)
+                    .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                    .foregroundColor(selectedTab == tab ? .green : .black)
+                    .font(.system(size: 21))
+                    .onTapGesture {
+                        withAnimation(.easeIn(duration: 0.1)) {
+                            selectedTab = tab
+                        }
+                    }
+                Spacer()
+            }
+        }
+        .frame(width: nil, height: 60)
+        .background(.thinMaterial)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar(selectedTab: .constant("house"))
+    }
+}
---
+++ MobileApp/Views/LandingView.swift
@@ -0,0 +1,38 @@
+//
+//  LandingView.swift
+//  MobileApp
+//
+//  Created by Sean She on 6/21/23.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    
+    @State private var selectedTab = "house"
+    
+    var body: some View {
+        TabView (selection: $selectedTab) {
+            Text("Home View")
+                .tag("house")
+            Text("Question")
+                .tag("questionmark")
+            Text("Chat")
+                .tag("bubble.left.and.bubble.right")
+            Text("History")
+                .tag("clock.arrow.circlepath")
+//            HomeView()
+//                .tag("house")
+
+        }
+        .overlay(alignment: .bottom) {
+            TabBar(selectedTab: $selectedTab)
+        }
+    }
+}
+
+struct LandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
